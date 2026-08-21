a678934711b95e6d319f74b8e12b815e52c8eae4
nd001-jyz <377725518@qq.com>
2026-08-21 09:22:11 +0800

    Deploy sw.js

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = 'naidou-v15';
+const ASSETS = ['./', './index.html', './manifest.webmanifest', './sw.js', './hanzi-data.js', './poems-data.js', './idioms-data.js', './en-data.js', './icon-192.png', './icon-512.png', './icon.svg'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      const fetched = fetch(event.request).then(resp => {
+        if (resp && resp.status === 200 && resp.type === 'basic') {
+          const copy = resp.clone();
+          caches.open(CACHE).then(c => c.put(event.request, copy));
+        }
+        return resp;
+      }).catch(() => cached);
+      return cached || fetched;
+    })
+  );
+});
